d779394bedef5d68ca5ec20a5f04bf1c1ec2fb28
Wu-HZ <102032485+Wu-HZ@users.noreply.github.com>
2026-05-08 20:19:11 +0800

    chore: allow cf tunnel host in vite dev server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+	allowedHosts: ['cf-tunnel.deaorui.top'],
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8001',
